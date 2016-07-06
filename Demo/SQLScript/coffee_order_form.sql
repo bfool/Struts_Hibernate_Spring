261c85b2f649226b761f7a9353f48bdd8a9cf5d3
purecaptain <purecaptain@users.noreply.github.com>
2016-07-06 19:45:26 +0800

    add SQL Script

---
+++ Demo/SQLScript/coffee_order_form.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE  IF NOT EXISTS `coffee` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+USE `coffee`;
+-- MySQL dump 10.13  Distrib 5.6.24, for osx10.8 (x86_64)
+--
+-- Host: 127.0.0.1    Database: coffee
+-- ------------------------------------------------------
+-- Server version	5.6.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `order_form`
+--
+
+DROP TABLE IF EXISTS `order_form`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order_form` (
+  `orderid` int(11) NOT NULL AUTO_INCREMENT,
+  `beanid` int(11) DEFAULT NULL,
+  `handleid` int(11) DEFAULT NULL,
+  `orderdate` datetime DEFAULT NULL,
+  `quantity` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`orderid`),
+  KEY `beanid_idx` (`beanid`),
+  KEY `handleid_idx` (`handleid`),
+  CONSTRAINT `beanid` FOREIGN KEY (`beanid`) REFERENCES `bean` (`beanid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `handleid` FOREIGN KEY (`handleid`) REFERENCES `handle` (`handleid`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-06-29 10:03:04
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Struts_Hibernate_Spring
 Some tests in the class.
 
-All tests' lib are empty.Using the /lib to put in it.
+All tests' lib are empty.Using the /libs to put in it.
 
 And the demo is the application for the class.
